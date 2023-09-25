39312713a6a7aa96891dd5ca3ac864f5de225e38
augusto21412lsdf <144640922+augusto21412lsdf@users.noreply.github.com>
2023-09-25 20:21:49 -0300

    primeiro commit

---
+++ conteudo/contatoConteudo.php
@@ -0,0 +1,23 @@
+<div id="conteudo">
+    <section id="formulario"> 
+    
+    <header> 
+        <h1>Entre em contato e logo iremos responder a sua dúvida</h1>
+        
+    </header> 
+    <fieldset> 
+        <legend> formulario de contato</legend>
+    <form name="frmContato" id="frmContato" method="get">
+        <input type="text" name="nome" id="nome" placeholder="digite seu nome">
+        <input type="email" name="email" id="email" placeholder="digite seu email">
+        <textarea name="duvida" id="duvida" placeholder="Descreva sua duvida"></textarea>
+        <button type="button" onclick="validaFormulario()">enviar</button> 
+        
+    </form> 
+    </fieldset>   
+    
+    
+    
+</div>
+    
+
